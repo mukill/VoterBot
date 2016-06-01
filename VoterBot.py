7a252d727fd4dda294baa4b16c44309bf07af94b
mukill <mukil.loganathan@gmail.com>
2016-06-01 10:41:53 -0700

    Style changes

--- VoterBot.py
+++ VoterBot.py
@@ -11,7 +11,7 @@ from datetime import datetime, timedelta
 # Important Values for the Bot
 NAME = "Voter_Help_Bot"
 BOT_KEY = "186316316:AAHeir7pHkLnwxEtw1Yn6M5Scac02iJIZOk"
-GET_ADDRESS, GET_CITY, GET_STATE,GET_ZIP, AWAIT_REGISTRATION, FINISHED, REMINDER_MODE = range(7)
+GET_ADDRESS, GET_CITY, GET_STATE, GET_ZIP, AWAIT_REGISTRATION, FINISHED, REMINDER_MODE = range(7)
 apiKey = "AIzaSyBeXcLqyIOqZkIMqGBVJmVLikBkC5QHh6c"
 searchEngineKey = '006560762859714176178:dkkf_njhddu'
 
@@ -34,12 +34,13 @@ def get_address(bot, update):
     user_id = update.message.from_user.id
     user_state = state.get(user_id, GET_ADDRESS)
 
-    state[user_id] = GET_CITY  # set the state
+    state[user_id] = GET_CITY# set the state
     bot.sendMessage(chat_id,
                         text="Please type in your house number and street",
                         reply_markup=ForceReply())
 
-#Message listener that changes depending on how much information the user has given the bot so far
+#Message listener that changes depending on how much information
+#the user has given the bot so far
 def bot_setup(bot, update):
     #Gather relevant message data
     chat_id = update.message.chat_id
@@ -52,7 +53,8 @@ def bot_setup(bot, update):
         state[chat_id] = GET_STATE
         # Save the user id and the answer to context
         context[chat_id] = update.message.text
-        bot.sendMessage(chat_id, text="Now enter your City", reply_markup= ForceReply())
+        bot.sendMessage(chat_id, text="Now enter your City",
+                        reply_markup= ForceReply())
 
     #Prompt user for their state
     elif chat_state == GET_STATE:
@@ -65,7 +67,8 @@ def bot_setup(bot, update):
         state_of_residence[user_id] = text
         context[chat_id] = context[chat_id] + ", " + text
         state[chat_id] = AWAIT_REGISTRATION
-        bot.sendMessage(chat_id, text="Now enter your Zip Code", reply_markup= ForceReply())
+        bot.sendMessage(chat_id, text="Now enter your Zip Code",
+                        reply_markup= ForceReply())
     #Prompt user to register to vote
     elif chat_state == AWAIT_REGISTRATION:
         addresses[user_id] = context[chat_id] + " " + text
@@ -73,8 +76,10 @@ def bot_setup(bot, update):
         state[chat_id] = FINISHED
         register_to_vote_markup = ReplyKeyboardMarkup([[KeyboardButton("Done Registering")]],
                                   one_time_keyboard = True)
-        bot.sendMessage(chat_id, text="Click this link: " + googleSearch('Register to Vote in %s' %state_of_residence[user_id]) + " to register to vote!",
-                                 reply_markup= register_to_vote_markup)
+        bot.sendMessage(chat_id, text="Click this link: "
+                        + googleSearch('Register to Vote in %s'
+                        % state_of_residence[user_id]) + " to register to vote!",
+                        reply_markup= register_to_vote_markup)
 
     #Give the user valuable information about the upcoming election if there is one
     elif chat_state == FINISHED:
@@ -83,56 +88,66 @@ def bot_setup(bot, update):
         try:
             state[chat_id] = REMINDER_MODE
             notificationsEnabled[chat_id] = True
-            electionName = electionDta['election']['name']
-            electionAddress[user_id] = electionDta['pollingLocations'][0]['address']['line1'] + ', ' + electionDta['pollingLocations'][0]['address']['city'] + ', ' + electionDta['pollingLocations'][0]['address']['state'] + ' ' + electionDta['pollingLocations'][0]['address']['zip']
+            loc = electionDta['pollingLocations'][0]['address']
+            electionAddress[user_id] = (loc['line1'] + ', ' + loc['city'] + ', '
+                                        + loc['state'] + ' ' + loc['zip'])
             electionDate[user_id] = electionDta['election']['electionDay']
             election_date_object = datetime.strptime(electionDate[user_id], '%Y-%m-%d')
-            responseText = "You are done! Your polling location is: %s Your election date is: %s I will send you a reminder every few days!" % (electionAddress[user_id], electionDate[user_id])
-            info_markup = ReplyKeyboardMarkup([[KeyboardButton("Election Info"), KeyboardButton("Disable Notifications"), KeyboardButton("Enable Notifications")]],
-                                      one_time_keyboard = False)
+            responseText = 'You are done! Your polling location is: %s.' \
+                            ' Your election date is: %s I will send you a ' \
+                            'reminder every few days!' % (electionAddress[user_id],
+                                                          electionDate[user_id])
+            info_markup = ReplyKeyboardMarkup([[KeyboardButton("Election Info"),
+                                                KeyboardButton("Disable Notifications"),
+                                                KeyboardButton("Enable Notifications")]],
+                                                one_time_keyboard = False)
             bot.sendMessage(chat_id, text= responseText, reply_markup = info_markup)
             #Function that will keep sending reminders to user
             def constantReminderFunction(bot):
-                reminderText = "Don't forget to vote! Your polling location is: %s Your election date is: %s. I will continue to send you a reminder every few days!" % (electionAddress[user_id], electionDate[user_id])
-                bot.sendMessage(chat_id, text= reminderText)
+                reminderText = 'Do not forget to vote! Your polling location' \
+                                ' is: %s. Your election date is: %s.' \
+                                ' I will continue to send you a reminder every' \
+                                ' few days!' % (electionAddress[user_id],
+                                                electionDate[user_id])
+                if (notificationsEnabled[chat_id]):
+                    bot.sendMessage(chat_id, text= reminderText)
                 now = datetime.now()
                 timeDiff = election_date_object - now
-                if timeDiff.total_seconds() > 60 * 60 * 24 * 3 and notificationsEnabled[chat_id]:
-                    job_queue.put(constantReminderFunction, 60 * 60 * 24 * 3, repeat=False)
+                if timeDiff.total_seconds() > 60 * 60 * 24 * 3:
+                    job_queue.put(constantReminderFunction,
+                                  60 * 60 * 24 * 3, repeat=False)
             #Function that will send reminder to user the day before the election
             def lastReminderFunction(bot):
-                reminderText = "Don't forget to vote tomorrow! Your polling location is: %s. This will be my last reminder!" % (electionAddress[user_id])
+                reminderText = 'Do not forget to vote tomorrow! Your polling ' \
+                                'location is: %s. This will be' \
+                                ' my last reminder!' % (electionAddress[user_id])
                 bot.sendMessage(chat_id, text= reminderText)
             now = datetime.now()
             timeDiff = election_date_object - now
             #Add the reminders to the job que
             job_queue.put(constantReminderFunction, 60 * 60 * 24 * 3, repeat=False)
-            job_queue.put(constantReminderFunction, timeDiff.total_seconds() - (60 * 60 * 24), repeat=False)
-        #Handle Errors when the GET Request does not execute properly due to various problems
+            job_queue.put(constantReminderFunction, timeDiff.total_seconds()
+                          - (60 * 60 * 24), repeat=False)
+        #Handle Errors when the GET Request does not execute properly
         except (KeyError):
             error = electionDta['error']['message']
             if error == 'Failed to parse address':
                 bot.sendMessage(chat_id, text="Invalid Address, use /set to try again")
             else:
-                bot.sendMessage(chat_id, text="You have no elections coming up! Make sure to check back and keep up with the news to vote in the future")
+                bot.sendMessage(chat_id, text='You have no elections coming up! ' \
+                                'Make sure to check back and keep up with the ' \
+                                'news to vote in the future')
     elif chat_state == REMINDER_MODE:
         if text == "Disable Notifications":
             notificationsEnabled[chat_id] = False
-            bot.sendMessage(chat_id, text= "Notifications Disabled. You will not receive a notification after your next one")
+            bot.sendMessage(chat_id, text= 'Notifications Disabled. '\
+                            'You will not receive a notification after your next one')
         elif text == "Enable Notifications":
             notificationsEnabled[chat_id] = True
             bot.sendMessage(chat_id, text= "Notifications Enabled")
-            election_date_object = datetime.strptime(electionDate[user_id], '%Y-%m-%d')
-            def constantReminderFunction(bot):
-                reminderText = "Don't forget to vote! Your polling location is: %s Your election date is: %s. I will continue to send you a reminder every few days!" % (electionAddress[user_id], electionDate[user_id])
-                bot.sendMessage(chat_id, text= reminderText)
-                now = datetime.now()
-                timeDiff = election_date_object - now
-                if timeDiff.total_seconds() > 60 * 60 * 24 * 3 and notificationsEnabled[chat_id]:
-                    job_queue.put(constantReminderFunction, 60 * 60 * 24 * 3, repeat=False)
-            job_queue.put(constantReminderFunction, 60 * 60 * 24 * 3 , repeat=False)
         elif text == "Election Info":
-            infotext = 'Your polling location is: %s Your election date is: %s' % (electionAddress[user_id],electionDate[user_id])
+            infotext = 'Your polling location is: %s Your election date ' \
+                        'is: %s' % (electionAddress[user_id],electionDate[user_id])
             bot.sendMessage(chat_id, text= infotext)
 
 #Method that uses the Google Custom Search API to search a query
@@ -153,11 +168,12 @@ def findVoterInfo(address):
 #Command listener for the help command that assists a user in getting started
 def help(bot, update):
     bot.sendMessage(update.message.chat_id,
-                                        text= "Hi, Type /set to reset the bot and get reminded to vote!")
+                    text= "Hi, Type /set to reset the bot and get reminded to vote!")
 #Command listener for the start command which begins the bot.
 def start(bot, update):
     bot.sendMessage(update.message.chat_id,
-                                        text= "Hi, I am VoterBot, here to ensure you don't forget to vote on time! Type /set to begin!")
+                    text= 'Hi, I am VoterBot, here to ensure you do not forget'\
+                     ' to vote on time! Type /set to begin!')
 
 #Error listener that logs errors
 def error(bot, update, error):
